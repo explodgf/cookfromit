dc44921ecedcb78f7957d56be6225e9f34a6181c
jakubwirfel <jakubwirfel@gmail.com>
2020-06-06 19:55:12 +0200

    Create arrow and arrow text. Add btn animation

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -3,7 +3,11 @@
         <div class="hero-inner">
             <h1>Cook from it</h1>
             <h2>Cook with ingredients from your fridge</h2>
-            <a href="#" class="hero-btn"></a>
+            <a href="#" class="hero-btn"><img src="public/assets/chef.svg"></a>
+            <div class="pointer">
+                <img src="public/assets/arrow.svg">
+                <h2>Find recipe</h2>
+            </div>
         </div>
     </section>
 <?php include 'inc/footer.php';?>
