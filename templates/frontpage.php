bd08574686ce962521fb7a24e41e3edd739f35d9
jakubwirfel <jakubwirfel@gmail.com>
2020-06-06 09:51:09 +0200

    frontpage in progress/ Td. arrow, arrow text

---
+++ templates/frontpage.php
@@ -0,0 +1,9 @@
+<?php include 'inc/header.php';?>
+    <section class="hero">
+        <div class="hero-inner">
+            <h1>Cook from it</h1>
+            <h2>Cook with ingredients from your fridge</h2>
+            <a href="#" class="hero-btn"></a>
+        </div>
+    </section>
+<?php include 'inc/footer.php';?>
