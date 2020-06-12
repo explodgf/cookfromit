f34f52439a25359855b1d1b19e7345e89e7c5bd2
jakubwirfel <jakubwirfel@gmail.com>
2020-06-12 17:26:30 +0200

    AStart creating recipe-view, add folder with icons

---
+++ templates/recipeview.php
@@ -0,0 +1,52 @@
+<?php include 'inc/header.php';?>
+    <main class="main">
+        <section class="recipe recipe-container">
+            <div class="info-box">
+                <div class="container">
+                    <h1>Potatoes with peppers in sauces</h1>
+                    <h2>Author: Jakub Wirfel</h2>
+                    <div class="info-container">
+                        <div class="icon-box">
+                            <img src="public/assets/icons8593ae/speed.svg" alt="icon" class="first"/>
+                        </div>
+                        <p>45 min</p>
+                        <div class="icon-box">
+                            <img src="public/assets/icons8593ae/medium.svg" alt="icon"/>
+                        </div>
+                        <p>Medium</p>
+                        <div class="icon-box">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="28.827" height="27.666" viewBox="0 0 28.827 27.666" id="like">
+                                <g id="like" transform="translate(0.5 0.5)">
+                                    <g id="Group_12" data-name="Group 12" transform="translate(0 11.594)">
+                                    <g id="Group_11" data-name="Group 11" transform="translate(0 0)">
+                                        <path id="Path_54" data-name="Path 54" d="M2.9,224A2.9,2.9,0,0,0,0,226.9v9.275a2.9,2.9,0,0,0,2.9,2.9H6.377a2.878,2.878,0,0,0,1.739-.587V224Z" transform="translate(0 -224)" stroke="#8593ae" stroke-width="1"/>
+                                    </g>
+                                    </g>
+                                    <g id="Group_14" data-name="Group 14" transform="translate(9.275)">
+                                    <g id="Group_13" data-name="Group 13" transform="translate(0 0)">
+                                        <path id="Path_55" data-name="Path 55" d="M189.217,26.609a2.571,2.571,0,0,0-.755-1.825,2.879,2.879,0,0,0,.742-2.213,3,3,0,0,0-3.024-2.628h-7.162a16.669,16.669,0,0,0,.923-4.638c0-2.515-2.137-4.638-3.478-4.638a3.647,3.647,0,0,0-2.1.706.583.583,0,0,0-.217.453v3.931l-3.339,7.233-.139.071V35.488a7.6,7.6,0,0,0,2.9.685h10.642a2.667,2.667,0,0,0,2.627-2.025,2.608,2.608,0,0,0-.21-1.741,2.6,2.6,0,0,0,1.161-3.479A2.6,2.6,0,0,0,189.217,26.609Z" transform="translate(-170.667 -10.667)"  stroke="#8593ae" stroke-width="1"/>
+                                    </g>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
+                        <p>1200</p>
+                        <div class="icon-box">
+                            <img src="public/assets/icons8593ae/social.svg" alt="icon"/>
+                        </div>
+                        <p>4</p>
+                    </div>
+                    <div class="ingredients">
+
+                    </div>
+                </div>
+            </div>
+            <div class="line"></div>
+            <div class="steps-img-box">
+                <div class="container">
+
+                </div>
+            </div>
+        </section>
+    </main>
+<script> </script> <!--BUG taransition fix for Chrome-->
