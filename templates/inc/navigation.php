af9acccfbacd538b6839d67578f4362f1c6d87ac
jakubwirfel <jakubwirfel@gmail.com>
2020-06-22 21:30:24 +0200

    nav logo

--- templates/inc/navigation.php
+++ templates/inc/navigation.php
@@ -1,7 +1,7 @@
 <nav class="navigation">
     <div class="nav-box">
         <div class="logo-box">
-
+            <img src="public/assets/Logo.svg" alt="logo"/>
         </div>
         <div class="items-box">
 
