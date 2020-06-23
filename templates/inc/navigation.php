44a947d728d7547eca64f72da6cc66dbb0245872
jakubwirfel <jakubwirfel@gmail.com>
2020-06-23 20:40:46 +0200

    login form created, menu modal to do

--- templates/inc/navigation.php
+++ templates/inc/navigation.php
@@ -1,10 +1,84 @@
 <nav class="navigation">
     <div class="nav-box">
         <div class="logo-box">
-            <img src="public/assets/Logo.svg" alt="logo"/>
+            <a href="index.php"><img src="public/assets/Logo.svg" alt="logo"/></a>
         </div>
         <div class="items-box">
-
+            <ul>
+                <li><a href="#login">Sign in</a></li>
+                <li><a href="registration.php" class="border_last">Sign up</a></li>
+                <li class="menu-icon"><a href="#menu">
+                    Menu
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="20" viewBox="0 0 24 20">
+                        <g id="Menu_Button" data-name="Menu Button" transform="translate(-1853 -71)">
+                            <rect id="Rectangle_3" data-name="Rectangle 3" width="24" height="4" rx="2" transform="translate(1853 71)" fill="#F5F1F1"/>
+                            <rect id="Rectangle_4" data-name="Rectangle 4" width="24" height="4" rx="2" transform="translate(1853 87)" fill="#F5F1F1"/>
+                            <rect id="Rectangle_5" data-name="Rectangle 5" width="16" height="4" rx="2" transform="translate(1853 79)" fill="#F5F1F1"/>
+                        </g>
+                    </svg>
+                </a></li>
+            </ul>
         </div>
     </div>
 </nav>
+<div id="login" class="modal-window">
+    <div>
+        <a href="#" title="Close" class="modal-close">Close</a>
+        <div class="modal-header">
+            <h1>Login</h1>
+        </div>
+            <form class="form-login" accept-charset="UTF-8">
+                <label class="label-login">
+                    Username / email
+                </label>
+                <div class="field-wrap-login">
+                    <input class="input-login" type="text" required autocomplete="off"/>
+                </div>
+                <label class="label-login">
+                    Password
+                </label>
+                <div class="field-wrap-login">
+                    <input class="input-login" type="password" required autocomplete="off"/>
+                </div>
+                <p class="forgot-login"><a href="#">Forgot Password?</a></p>
+                <button type="submit" class="button-login">Login</button>
+            </form>
+    </div>
+    <div class="modal-footer">
+        <div class="footer-box">
+                <div class="copyright">
+                    <div class="dot-copyright"><i class="far fa-copyright"></i></div>
+                    <p>Copyright 2020, Cook From It</p>
+                </div>
+                <div class="socials">
+                    <p>Follow us:</p>
+                    <div class="socials-icons">
+                        <i class="fab fa-facebook"></i>
+                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-snapchat"></i>
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+<div id="menu" class="menu">
+    <div>
+
+    </div>
+    <div class="modal-footer">
+        <div class="footer-box">
+                <div class="copyright">
+                    <div class="dot-copyright"><i class="far fa-copyright"></i></div>
+                    <p>Copyright 2020, Cook From It</p>
+                </div>
+                <div class="socials">
+                    <p>Follow us:</p>
+                    <div class="socials-icons">
+                        <i class="fab fa-facebook"></i>
+                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-snapchat"></i>
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
